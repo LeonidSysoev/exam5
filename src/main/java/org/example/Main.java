e97e226ea7d9c1ebffbd16c4772bf69a16075b9f
LeonidHome <defenser2205@gmail.com>
2023-08-25 00:18:54 +0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
-
+// Есть таблица employee с колонками name, department, salary.
+// Вывести максимальную и среднюю зарплату (salary) по каждому отделу,
+// имеющему больше одного сотрудника.
 
 public class Main {
     public static void main(String[] args) {
-
 
     }
 }
